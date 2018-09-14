5fdb5eccb962b7941a99ab5db62b0f54fbddbc5e
Mutukuri Rajasekhar <sekhar.mutukuri@gmail.com>
2018-09-14 12:42:27 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  
+    firebase: 'maps-2f553',
+    mapbox: {
+        accessToken: 'pk.eyJ1IjoibWF0dGtldG1vIiwiYSI6IjZtQ2ZDRFUifQ.cRea26bUW1FA-hxTL5fDzA',
+        mapId: 'mattketmo.locpi8o0',
+    },
+}
